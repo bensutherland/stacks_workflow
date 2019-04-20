ad1530727a9bafebcb2765b3c0647646a0116b62
bensutherland <bensutherland7@gmail.com>
2019-04-20 08:46:25 -0700

    Add user defined variables and fix report name

--- 00-scripts/stacks2_1_gstacks.sh
+++ 00-scripts/stacks2_1_gstacks.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 # Launch gstacks to build loci from aligned data 
 # The input BAM file(s) must be sorted by coordinate
-TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 
-# User set variables
+# Test if user specified a number of CPUs
+if [[ -z "$NCPU" ]]
+then
+    NCPU=1
+fi
+
+# Global variables 
+NCPU=$1
+
+# User defined variables
 map="01-info_files/population_map_retained.txt"
-NCPU="8"
 
 # Copy script as it was run
+TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 SCRIPT=$0
 NAME=$(basename $0)
 LOG_FOLDER="10-log_files"
 
 cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
+
 
 ## Options (comment out any options you do not want to use
 # General options:
@@ -30,7 +39,7 @@ min_mapq="--min-mapq 10"          # minimum PHRED-scaled mapping quality to cons
 max_clipped="--max-clipped 0.20"  # maximum soft-clipping level, in fraction of read length (default: 0.20)
 max_insert_len="--max-insert-len 1000" # maximum allowed sequencing insert length (default: 1000)
 details="--details"               # write a heavier output
-#phasing_coocurrences_thr_range="--phasing-cooccurrences-thr-range 1,2" # range of edge coverage thresholds to iterate over when building the graph of allele cooccurrences for SNP phasing (default: 1,2)
+phasing_coocurrences_thr_range="--phasing-cooccurrences-thr-range 1,2" # range of edge coverage thresholds to iterate over when building the graph of allele cooccurrences for SNP phasing (default: 1,2)
 #phasing_dont_prune_hets="--phasing-dont-prune-hets" # don't try to ignore dubious heterozygote genotypes during phasing
 
 # Launch gstacks for all the individuals
@@ -43,5 +52,5 @@ do
         $min_mapq $max_clipped $max_insert_len \
         $details $phasing_coocurrences_thr_range $phasing_dont_prune_hets 
     id=$(echo $id + 1 | bc)
-done 2>&1 | tee 10-log_files/"$TIMESTAMP"_stacks_1b_pstacks.log
+done 2>&1 | tee 10-log_files/"$TIMESTAMP"_stacks2_gstacks.log
 
